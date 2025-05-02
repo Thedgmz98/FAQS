68f39a4e30cf53e6ee27547c20b71d0e53b6fe54
Thedgmz98 <endoflong987@gmail.com>
2025-05-02 11:28:25 -0500

    Faq

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function btn(id){
+    const chechi = document.getElementById(id);
+    if (chechi.style.display == "none"){
+        chechi.style.display = "block";
+    } else{
+        chechi.style.display = "none";
+    }
+    console.log("toggle Worked")
+
+
+    // codo to change de incon 
+    
+}
